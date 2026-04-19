9f42bc08c387e57c7c919caa9c897ff05e9f821f
Youssef Shemela <youssefshemela12@gmail.com>
2026-04-19 23:49:58 +0200

    Initialize ballbot_health_monitor package

---
+++ src/ballbot_health_monitor/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'ballbot_health_monitor'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='youssef-shemela',
+    maintainer_email='youssefshemela12@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
